5b79d765b7acddc08316dc5a23400b2e6d667f96
Ani-wolf <83391971+Ani-wolf@users.noreply.github.com>
2021-05-14 17:57:51 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,15 @@
-// Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDdYrOv1XFHgd7tOzLL6IhFVemkWU7uVB0",
-  authDomain: "kwitterhomework.firebaseapp.com",
-  databaseURL: "https://kwitterhomework-default-rtdb.firebaseio.com",
-  projectId: "kwitterhomework",
-  storageBucket: "kwitterhomework.appspot.com",
-  messagingSenderId: "154722441008",
-  appId: "1:154722441008:web:d5072e177a6035b9c51af5"
-};
-// Initialize Firebase
-  
-  
+    apiKey: "AIzaSyCUbNnNJBoyZYVxbWeq4Qld3pDkI81DQlU",
+    authDomain: "kwitter-c5c9b.firebaseapp.com",
+    databaseURL: "https://kwitter-c5c9b-default-rtdb.firebaseio.com",
+    projectId: "kwitter-c5c9b",
+    storageBucket: "kwitter-c5c9b.appspot.com",
+    messagingSenderId: "366547473660",
+    appId: "1:366547473660:web:8a3606407eb2840d0d7379"
+  };
+function add_user() {
+    username=document.getElementById("user_name").value;
+    firebase.database().ref("/").child(username).update({
+        purpose:"adding_user"
+    });
+}
